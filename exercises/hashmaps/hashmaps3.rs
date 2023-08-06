735998adbccddf105b64b961b106fd78f2cdf734
Rodolfo De Nadai <rdenadai@gmail.com>
2023-08-06 10:26:50 -0300

    feat: 2 new exercices and 1 correction;

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,14 +14,33 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
 struct Team {
     goals_scored: u8,
     goals_conceded: u8,
+}
+
+fn mutable_hashmap(
+    mut scores: &mut HashMap<String, Team>,
+    team_name: String,
+    goals_scored: u8,
+    goals_conceded: u8,
+) {
+    if scores.get(&team_name).is_none() {
+        scores.insert(
+            team_name.clone(),
+            Team {
+                goals_scored,
+                goals_conceded,
+            },
+        );
+    } else {
+        let mut team = scores.get_mut(&team_name).unwrap();
+        team.goals_scored += goals_scored;
+        team.goals_conceded += goals_conceded;
+    }
 }
 
 fn build_scores_table(results: String) -> HashMap<String, Team> {
@@ -39,6 +58,8 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        mutable_hashmap(&mut scores, team_1_name, team_1_score, team_2_score);
+        mutable_hashmap(&mut scores, team_2_name, team_2_score, team_1_score);
     }
     scores
 }
--- exercises/vecs/vecs2.rs
+++ exercises/vecs/vecs2.rs
@@ -11,7 +11,7 @@ fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
     for element in v.iter_mut() {
         // TODO: Fill this up so that each element in the Vec `v` is
         // multiplied by 2.
-        *i = *i * 2;
+        *element = *element * 2
     }
 
     // At this point, `v` should be equal to [4, 8, 12, 16, 20].
@@ -19,11 +19,6 @@ fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
 }
 
 fn vec_map(v: &Vec<i32>) -> Vec<i32> {
-    v.iter().map(|element| {
-        // TODO: Do the same thing as above - but instead of mutating the
-        // Vec, you can just return the new number!
-        ???
-    }).collect()
     v.iter()
         .map(|element| {
             // TODO: Do the same thing as above - but instead of mutating the
